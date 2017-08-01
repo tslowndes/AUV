e121d96bf133431713634efe3da3fa7138da4358
tslowndes <tom@cdsat.co.uk>
2017-08-01 16:01:58 +0100

    More testing

--- tests/test_plant.py
+++ tests/test_plant.py
@@ -3,13 +3,13 @@ import pytest
 import sys
 sys.path.insert(0, "../../gen")
 from Config_class import *
-import numpy as np
 
 @pytest.mark.parametrize("yaw,yaw_demand,expected", [
     (330,10,1),
     (10,330,0),
     (180,359,1),
-    (178,359,0)
+    (178,359,0),
+    (180,360,1)
 ])
 
 def test_yaw_logic(yaw, yaw_demand, expected):
@@ -26,3 +26,56 @@ def test_yaw_logic(yaw, yaw_demand, expected):
         assert AUV.yaw > yaw
     elif expected == 0:
         assert AUV.yaw < yaw
+
+
+@pytest.mark.parametrize("start_loc, wayp, expected", [
+    ((0,0,-50), (50,50,0), 0),
+    ((0, 0, 0), (50, 50, -50), 1),
+    ((0,0,0), (50,50,0), 2)
+])
+
+def test_pitch(start_loc, wayp, expected):
+    AUV = Vehicle(0, 1, start_loc[0], start_loc[1], start_loc[2], 0)
+    config = sim_config('config/sim_config.csv')
+
+    AUV.waypoints = [wayp]
+    AUV.current_waypoint = 0
+
+    AUV.move_to_waypoint()
+    AUV.plant(config.time_step)
+
+    if expected == 1:
+        # assert AUV pitches nose down
+        assert AUV.pitch > 0
+    elif expected == 0:
+        # assert AUV pitches nose up
+        assert AUV.pitch < 0
+    elif expected == 2:
+        # assert AUV pitches nose up
+        assert AUV.pitch == 0
+
+@pytest.mark.parametrize("v,v_demand,expected", [
+    (1,0.5,0),
+    (0.5, 1, 1),
+    (1,1.25,2),
+    (0.25,0,2)
+])
+
+
+def test_v(v, v_demand, expected):
+    AUV = Vehicle(0, 1, 0, 0, 0, 0)
+    config = sim_config('config/sim_config.csv')
+
+    AUV.set_v(v)
+    AUV.set_v_demand(v_demand)
+    AUV.plant(config.time_step)
+
+    if expected == 1:
+        # assert AUV pitches nose down
+        assert AUV.v > v
+    elif expected == 0:
+        # assert AUV pitches nose up
+        assert AUV.v < v
+    elif expected == 2:
+        # assert AUV pitches nose up
+        assert AUV.v == v
