a13ed92e28a620c07746239ec8e7aff01357d3c8
tslowndes <tom@cdsat.co.uk>
2017-08-01 15:17:10 +0100

    Setting up for pytest and pytest-repeat

--- tests/test_dead_reckoner.py
+++ tests/test_dead_reckoner.py
@@ -5,78 +5,68 @@ sys.path.insert(0, "../../gen")
 from Config_class import *
 import numpy as np
 
-def repeat(times):
-    def repeatHelper(f):
-        def callHelper(*args):
-            for i in range(0, times):
-                f(*args)
-
-        return callHelper
-
-    return repeatHelper
-
-class test_pitch_0_yaw_0(unittest.TestCase):
-    def setUp(self):
-        # (self,i, Swarm_Size, start_x, start_y, start_z, start_yaw)
-        self.AUV = Vehicle(0, 1, 0, 0, 0, 0)
-        self.config = sim_config('config/sim_config.csv')
-
-    def test_go(self):
-        print('Testing yaw = %i degrees and pitch = %i' % (self.AUV.yaw,self.AUV.pitch))
-        self.AUV.dead_reckoner(self.config.time_step)
-        assert self.AUV.x == 0.25
-        assert self.AUV.y == 0
-
-    def tearDown(self):
-        del self.AUV
-
-class test_pitch_0_yaw_90(unittest.TestCase):
-    def setUp(self):
-        self.AUV = Vehicle(0, 1, 0, 0, 0, 0)
-        self.config = sim_config('config/sim_config.csv')
-        self.AUV.set_yaw(90)
-
-    def test_go(self):
-        print('Testing yaw = %i degrees and pitch = %i' % (self.AUV.yaw,self.AUV.pitch))
-        self.AUV.dead_reckoner(self.config.time_step)
-        # Due to precision of pi x is not exactly 0
-        self.assertAlmostEqual(self.AUV.x, 0)
-        assert self.AUV.y == 0.25
-
-    def tearDown(self):
-        del self.AUV
-
-class test_pitch_0_yaw_180(unittest.TestCase):
-    def setUp(self):
-        # (self,i, Swarm_Size, start_x, start_y, start_z, start_yaw)
-        self.AUV = Vehicle(0, 1, 0, 0, 0, 0)
-        self.config = sim_config('config/sim_config.csv')
-        self.AUV.set_yaw(180)
-
-    def test_go(self):
-        print('Testing yaw = %i degrees and pitch = %i' % (self.AUV.yaw,self.AUV.pitch))
-        self.AUV.dead_reckoner(self.config.time_step)
-        assert self.AUV.x == -0.25
-        self.assertAlmostEqual(self.AUV.y, 0)
-
-    def tearDown(self):
-        del self.AUV
-
-class test_pitch_0_yaw_270(unittest.TestCase):
-    def setUp(self):
-        # (self,i, Swarm_Size, start_x, start_y, start_z, start_yaw)
-        self.AUV = Vehicle(0, 1, 0, 0, 0, 0)
-        self.config = sim_config('config/sim_config.csv')
-        self.AUV.set_yaw(270)
-
-    def test_go(self):
-        print('Testing yaw = %i degrees and pitch = %i' % (self.AUV.yaw,self.AUV.pitch))
-        self.AUV.dead_reckoner(self.config.time_step)
-        self.assertAlmostEqual(self.AUV.x, 0)
-        assert self.AUV.y == -0.25
-
-    def tearDown(self):
-        del self.AUV
+# class test_pitch_0_yaw_0(unittest.TestCase):
+#     def setUp(self):
+#         # (self,i, Swarm_Size, start_x, start_y, start_z, start_yaw)
+#         self.AUV = Vehicle(0, 1, 0, 0, 0, 0)
+#         self.config = sim_config('config/sim_config.csv')
+#
+#     def test_go(self):
+#         print('Testing yaw = %i degrees and pitch = %i' % (self.AUV.yaw,self.AUV.pitch))
+#         self.AUV.dead_reckoner(self.config.time_step)
+#         assert self.AUV.x == 0.25
+#         assert self.AUV.y == 0
+#
+#     def tearDown(self):
+#         del self.AUV
+#
+# class test_pitch_0_yaw_90(unittest.TestCase):
+#     def setUp(self):
+#         self.AUV = Vehicle(0, 1, 0, 0, 0, 0)
+#         self.config = sim_config('config/sim_config.csv')
+#         self.AUV.set_yaw(90)
+#
+#     def test_go(self):
+#         print('Testing yaw = %i degrees and pitch = %i' % (self.AUV.yaw,self.AUV.pitch))
+#         self.AUV.dead_reckoner(self.config.time_step)
+#         # Due to precision of pi x is not exactly 0
+#         self.assertAlmostEqual(self.AUV.x, 0)
+#         assert self.AUV.y == 0.25
+#
+#     def tearDown(self):
+#         del self.AUV
+#
+# class test_pitch_0_yaw_180(unittest.TestCase):
+#     def setUp(self):
+#         # (self,i, Swarm_Size, start_x, start_y, start_z, start_yaw)
+#         self.AUV = Vehicle(0, 1, 0, 0, 0, 0)
+#         self.config = sim_config('config/sim_config.csv')
+#         self.AUV.set_yaw(180)
+#
+#     def test_go(self):
+#         print('Testing yaw = %i degrees and pitch = %i' % (self.AUV.yaw,self.AUV.pitch))
+#         self.AUV.dead_reckoner(self.config.time_step)
+#         assert self.AUV.x == -0.25
+#         self.assertAlmostEqual(self.AUV.y, 0)
+#
+#     def tearDown(self):
+#         del self.AUV
+#
+# class test_pitch_0_yaw_270(unittest.TestCase):
+#     def setUp(self):
+#         # (self,i, Swarm_Size, start_x, start_y, start_z, start_yaw)
+#         self.AUV = Vehicle(0, 1, 0, 0, 0, 0)
+#         self.config = sim_config('config/sim_config.csv')
+#         self.AUV.set_yaw(270)
+#
+#     def test_go(self):
+#         print('Testing yaw = %i degrees and pitch = %i' % (self.AUV.yaw,self.AUV.pitch))
+#         self.AUV.dead_reckoner(self.config.time_step)
+#         self.assertAlmostEqual(self.AUV.x, 0)
+#         assert self.AUV.y == -0.25
+#
+#     def tearDown(self):
+#         del self.AUV
 
 class test_pitch_yaw(unittest.TestCase):
     def setUp(self):
