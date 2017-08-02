e3763990fe7a5010cafeb8968f8257fbca3cc2bd
tslowndes <tom@cdsat.co.uk>
2017-08-02 15:30:07 +0100

    Waypoint following testing - highlighted 200m restriction issue

--- tests/test_waypoint_following.py
+++ tests/test_waypoint_following.py
@@ -39,3 +39,22 @@ def test_pitch_demand(start, target, expected):
     AUV.waypoints = [target]
     AUV.move_to_waypoint()
     assert abs(AUV.pitch_demand - expected) < 0.0000001
+
+@pytest.mark.parametrize("state, start, target, expected", [
+    (0, (0, 0, 0), [100, 100], (100, 100, -50)),
+    (1, (0, 0, -50), [100, 100], (100, 100, 0)),
+    (0, (0, 0, 0), [200, 200], (141.421356237, 141.421356237, -50)),
+    (1, (0, 0, 0), [200, 200], (141.421356237, 141.421356237, 0)),
+    (2, (0, 0, 0), [200, 200], (0, 0, 0)),
+])
+
+def test_waypoint_setting(state, start, target, expected):
+    # (self,i, Swarm_Size, start_x, start_y, start_z, start_yaw)
+    AUV = Vehicle(0, 1, start[0], start[1], start[2], 0)
+    config = sim_config('config/sim_config.csv')
+    AUV.state = state
+    AUV.set_waypoint(target, config)
+
+    assert (abs(AUV.waypoints[0][0] - expected[0]) < 0.0000001 and
+            abs(AUV.waypoints[0][1] - expected[1]) < 0.0000001 and
+            abs(AUV.waypoints[0][2] - expected[2]) < 0.0000001)
