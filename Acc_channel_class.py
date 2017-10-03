b779b6e08451b9837cf1e499db4374e62cf96173
Unknown <tom@cdsat.co.uk>
2017-10-03 16:56:16 +0100

    lat lon conversion for acoustic

--- Acc_channel_class.py
+++ Acc_channel_class.py
@@ -1,10 +1,13 @@
+import sys
+sys.path.insert(0, "../gen")
 import numpy as np
+from latlon_util import find_dist3
 
 class V2V_comms:
     def transmit_msg(self, vehicles, config):
         self.receive_msg = np.zeros(config.swarm_size)
-        vehicle_pos = np.array([[AUV.x, AUV.y, AUV.z] for AUV in vehicles])
-        dists = np.sqrt(np.sum((vehicle_pos - self.msg.pos)**2, axis = 1))
+        vehicle_pos = np.array([[AUV.lon, AUV.lat, AUV.z] for AUV in vehicles])
+        dists = np.array([find_dist3(pos, self.msg.pos) for pos in vehicle_pos])
         self.receive_msg[np.where(dists < config.comms_range)[0]] = 1
         self.receive_msg[self.msg.ID] = 0
 
